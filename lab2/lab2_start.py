fcd5d6f973b15a78dc0b00eed0e2f78cd4e135d8
jeljov <jeljov@gmail.com>
2024-03-06 23:20:47 +0100

    tasks for Lab2 added

---
+++ lab2/lab2_start.py
@@ -0,0 +1,48 @@
+
+
+
+if __name__ == '__main__':
+
+    pass
+
+    # Zadatak 1
+    # list1 = ["M", "na", "i", "Ke"]
+    # list2 = ["y", "me", "s", "lly"]
+    # print(povezi_po_indeksima(list1, list2))
+
+    # Zadatak 2:
+    # s = "Tokyo's 2024 population is now estimated at 37,115,035."
+    # print(cifre_u_stringu(s))
+
+    # Zadatak 3
+    # s1 = "potop"
+    # print(palindrom(s1))
+    # s2 = "Sir ima miris"
+    # print(palindrom(s2))
+    # s3 = "ananas"
+    # print(palindrom(s3))
+
+    # Zadatak 4:
+    # lozinke_za_proveru = "ABd1234@1, a F1#, 2w3E*, 2We334#5, t_456WR"
+    # print(f"Passwords to check: {lozinke_za_proveru}")
+    # provera_lozinki(lozinke_za_proveru)
+
+    # Zadatak 5:
+    # sve_parne_cifre()
+
+    # Zadatak 6:
+    # print(anagram('ortoped', 'torpedo'))
+    # print(anagram('ortopedi', 'torpedo'))
+    # print(anagram('On sa tla Like', 'Nikola Tesla'))
+
+
+    # Zadatak 7:
+    # izvestaj = '''
+    #     Server abc01 is up
+    #     Server abc02 is down
+    #     Server abc03 is down
+    #     Server xyz01 is up
+    #     Server xyz02 is up
+    #     Server abc02 is up
+    #     '''
+    # stanje_servera(izvestaj)
