ffca9aaa885f2f0aece6a06caea2c568e9c1cfe9
PetarLakcevic <petar.lakcevic@gmail.com>
2024-03-10 23:50:29 +0100

    Lab 03 added

---
+++ lab3/lab3_start.py
@@ -0,0 +1,59 @@
+
+
+
+if __name__ == '__main__':
+
+    pass
+
+    # # Task 1
+    # create_print_numeric_dict(7)
+    # print()
+    #
+    # # Task 2
+    # dishes = ["pizza", "sauerkraut", "paella", "hamburger"]
+    # countries = ["Italy", "Germany", "Spain", "USA", "Serbia"]
+    # lists_to_dict(countries, dishes)
+    # print()
+    #
+    # # Task 3
+    # print("string_stats('Today is October 24, 2023!'):")
+    # print(string_stats("Today is October 24, 2023!"))
+    # print()
+    #
+    # # Task 4
+    # sample_websites = ['https://www.technologyreview.com/', 'https://www.tidymodels.org/',
+    #                    'https://podcasts.google.com/', 'https://www.jamovi.org/', 'http://bg.ac.rs/']
+    #
+    # print(website_stats(sample_websites))
+    # print()
+    #
+    # # Task 5
+    # # response by GPT-3 to the question of why it has so entranced the tech community
+    # # source: https://www.wired.com/story/ai-text-generator-gpt-3-learning-language-fitfully/
+    # gpt3_response = ("""
+    #     I spoke with a very special person whose name is not relevant at this time,
+    #     and what they told me was that my framework was perfect. If I remember correctly,
+    #     they said it was like releasing a tiger into the world.
+    # """)
+    # token_frequency(gpt3_response)
+    # print()
+    #
+    # # Task 6:
+    # print("Passwords to check: ABd1234@1, a F1#, 2w3E*, 2We334#5, t_456WR")
+    # validation_dict = password_check("ABd1234@1, a F1#,2w3E*,2We334#5, t_456WR")
+    # print("Validation results:")
+    # for password, result in validation_dict.items():
+    #     print(f"- {password}: {', '.join(result)}")
+    # print()
+    #
+    # # Task 7:
+    # l = [('V', 1), ('VI', 1), ('V', 2), ('VI', 2), ('VI', 3), ('VII', 1)]
+    # classroom_stats(l)
+    # print()
+    #
+    # # Task 8:
+    # # team = [{'name': 'Bob', 'age': 18, 'score': 50.0},
+    # #         {'name': 'Tim', 'age': 17, 'score': 84.0},
+    # #         {'name': 'Jim', 'age': 22, 'score': 94.0},
+    # #         {'name': 'Joe', 'age': 19, 'score': 85.5}]
+    # team_stats(team)
