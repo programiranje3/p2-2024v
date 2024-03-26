85ae1d4ffa0abfc22405c55f45c304a5377ed637
jeljov <jeljov@gmail.com>
2024-03-26 07:26:23 +0400

    Lab 5 added

---
+++ lab5/let_start.py
@@ -0,0 +1,54 @@
+
+if __name__ == '__main__':
+
+    pass
+
+    # lh1411 = Let('LF1411', '2024-05-05 6:50')
+    # lh992 = Let('LH992', '2024-05-25 12:20')
+    #
+    # print("\nLETOVI:\n")
+    # print(lh1411)
+    # print()
+    # print(lh992)
+    # print()
+    #
+    # bob = Putnik("Bob Smith", "UK", "123456", True)
+    # john = Putnik("John Smith", "USA", 987656, True)
+    # anna = Putnik("Anna Smith", "Spain", "987659")
+    # luis = Putnik.covid_bezbedan_Francuz("Luis Bouve", "123654")
+    #
+    # print(f"\nDodavanje putnika na let {lh1411.broj_leta}")
+    # for p in [bob, john, anna, luis]:
+    #     lh1411.dodaj_putnika(p)
+    #
+    # print(f"\nPokusaj dodavanja putnika koji je vec u listi putnika za let {lh1411.broj_leta}:")
+    # lh1411.dodaj_putnika(Putnik("J Smith", "USA", "987656", True))
+    # print()
+    #
+    # print(f"\nPodaci o letu {lh1411.broj_leta} nakon dodavanja putnika na let:\n")
+    # print(lh1411)
+    #
+    # print()
+    #
+    # do_poletanja = lh1411.vreme_do_poletanja()
+    # if do_poletanja:
+    #     dani, sati, mins = do_poletanja
+    #     print(f"Vreme preostalo do poletanja leta {lh1411.broj_leta}: "
+    #           f"{dani} dana, {sati} sati, i {mins} minuta")
+    #
+    # print()
+    #
+    # print("\nPUTNICI NA LETU LH1411 (iter / next):")
+    # p_iter = iter(lh1411)
+    # try:
+    #     while True:
+    #         print(next(p_iter))
+    # except StopIteration:
+    #     print("Svi putnici su izlistani")
+    #
+    # print()
+    # print("\nPUTNICI NA LETU LH1411 (FOR petlja):")
+    # for p in iter(lh1411):
+    #     print(p)
+
+
---
+++ lab5/putnik_start.py
@@ -0,0 +1,29 @@
+if __name__ == '__main__':
+
+    pass
+
+    # bob = Putnik("Bob Smith", "UK", "123456", True)
+    # john = Putnik("John Smith", "USA", 987656, True)
+    # anna = Putnik("Anna Smith", "Spain", "987659")
+    # luis = Putnik.covid_bezbedan_Francuz("Luis Bouve", "123654")
+    #
+    # print("PUTNICI:\n")
+    # print(bob)
+    # print(john)
+    # print(anna)
+    # print(luis)
+    #
+    # print("\nPUTNICI NAKON UPDATE-a COVID STATUS-a:\n")
+    # anna.azuriraj_COVID_bezbedan('vakcinacija', '01/02/2024')
+    # print(anna)
+    #
+    # luis.azuriraj_COVID_bezbedan('negativan_test', '20/03/2024')
+    # print(luis)
+    # print()
+    #
+    # print("Provera da li su 'bob' i 'john' reference na istog putnika")
+    # print("Isti putnik" if bob == john else "Razliciti putnici")
+    # print()
+    # print("Provera da li su 'john' i 'johnny' reference na istog putnika")
+    # johnny = Putnik("Johnny Smith", "USA", 987656, False)
+    # print("Isti putnik" if john == johnny else "Razliciti putnici")
