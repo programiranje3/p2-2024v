ef59611a8f482dc9ef7028f42b1c955d302f930c
jeljov <jeljov@gmail.com>
2024-04-16 18:05:45 +0200

    Tasks for Lab8

---
+++ lab8/lab8.py
@@ -0,0 +1,33 @@
+
+
+if __name__ == '__main__':
+
+    pass
+
+    # # Zadatak 1
+    # analiza_fajlova_sa_slikama(get_data_dir() / 'image_files_for_training.txt')
+    #
+    # zad1_recnik = deserijalizuj_podatke(get_results_dir() / 'zadatak1_dict.pkl')
+    # for ent, lista_slika in zad1_recnik.items():
+    #     print(f"{ent.upper()}: {', '.join(lista_slika)}")
+    #
+    # zad1_lista = ucitaj_iz_txt_fajla(get_results_dir() / 'zadatak1_stats.txt')
+    # for entity_stat in zad1_lista:
+    #     print(entity_stat)
+    #
+    # # Zadatak 2
+    # unos_podataka_o_timu()
+    #
+    # podaci_o_timu = ucitaj_iz_csv_fajla(get_results_dir() / 'podaci_o_timu.csv')
+    # for podaci_o_clanu in podaci_o_timu:
+    #     # pprint(podaci_o_clanu)
+    #     ime, godine, poeni = podaci_o_clanu.values()
+    #     print(f"{ime}, {godine} godine, {poeni} poena")
+    #
+    # # Zadatak 3
+    # f1 = get_data_dir() / 'happy_numbers.txt'
+    # f2 = get_data_dir() / 'prime_numbers.txt'
+    # zabelezi_presek_brojeva(f1, f2)
+    #
+    # zad1_recnik = deserijalizuj_podatke(get_results_dir() / 'zadatak3_rezultati.pkl')
+    # pprint(zad1_recnik)
